74cce0443bec7e19352bcb7bc0da7623b93d7167
SLH <114884726+SLH335@users.noreply.github.com>
2024-10-03 16:33:52 +0200

    feat: main page with date overview

--- http/index.handler.go
+++ http/index.handler.go
@@ -2,13 +2,22 @@ package http
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 
 	"github.com/slh335/hpi-mensa-api/components"
+	. "github.com/slh335/hpi-mensa-api/types"
 	"github.com/slh335/hpi-mensa-api/util"
 )
 
 func (server *Server) Index(c echo.Context) error {
-	return c.HTML(http.StatusOK, util.RenderComponent(components.Index()))
+	meals, err := server.MealService.Get(Location{Id: 9601}, German, time.Now())
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{
+			Success: false,
+			Message: "failed to load meals",
+		})
+	}
+	return c.HTML(http.StatusOK, util.RenderComponent(components.Index(meals)))
 }
